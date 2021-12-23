e84f6ba0e0d7be8af72a07d8648c423753c6fc52
rgurgul <gurgul.robert@gmail.com>
2021-12-23 22:34:34 +0100

    idb

---
+++ src/app/shared/services/cart.idb.service.ts
@@ -0,0 +1,41 @@
+import { CartItemModel } from './../models/store.models';
+import { Injectable } from '@angular/core';
+import { openDB, DBSchema, IDBPDatabase } from 'idb';
+
+enum Cart {
+    DBName = 'CART_DB',
+    ObjectStore = 'CART_ITEMS',
+    ObjectKey = 'ITEMS',
+}
+
+interface CartDB extends DBSchema {
+    [Cart.ObjectStore]: {
+        key: string;
+        value: CartItemModel[];
+    };
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CartIDBService {
+    db: IDBPDatabase<CartDB>;
+
+    private async connectDB() {
+        this.db = await openDB<CartDB>(Cart.DBName, 1, {
+            upgrade(db, version) {
+                db.createObjectStore(Cart.ObjectStore);
+            },
+        });
+    }
+
+    async get(): Promise<CartItemModel[]> {
+        await this.connectDB();
+        return this.db.get(Cart.ObjectStore, Cart.ObjectKey);
+    }
+
+    async update(payload = []): Promise<any> {
+        await this.connectDB();
+        return this.db.put(Cart.ObjectStore, payload, Cart.ObjectKey);
+    }
+}
